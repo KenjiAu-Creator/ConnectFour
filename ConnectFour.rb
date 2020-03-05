697916597d0114af3469176a6fee9cd147bd9c60
Kenji <kenji.au9@gmail.com>
2020-03-05 13:38:23 -0700

    Inital commit

---
+++ ConnectFour.rb
@@ -0,0 +1,81 @@
+class ConnectFour
+  def initialize
+    intro()
+    create_board()
+    createMoveHash()
+  end
+
+  def intro
+    puts "Connect Four is a two-player connection game in which players first choose"
+    puts "a color and then take turns dropping one colored disc from the top into seven-column"
+    puts "six-row vertically suspended grid. The pieces fall straight down, occupying the lowest space"
+    puts "in the column. The objective is to form a horizontal, vertical or diagonal line of four first."
+    puts "=============================================================================================="
+  end
+
+  def create_board
+    row = "| O | O | O | O | O | O | O |"
+    6.times do
+      puts row
+    end
+    puts "============================="
+  end
+
+  def createMoveHash()
+    @moves = {
+      1 => [],
+      2 => [],
+      3 => [],
+      4 => [],
+      5 => [],
+      6 => [],
+      7 => [],
+    }
+  end
+
+  def winCondition
+    
+  end
+
+  def getMove()
+    puts "Please enter the column which you wish to place the marker."
+    playerMove = gets.chomp
+    if (playerMove.to_i > 7 || playerMove.to_i < 1)
+      puts "Invalid column. Please enter a column between 1-7 to play."
+      getMove()
+    else
+      return playerMove
+    end
+  end
+
+  def placeMove(marker, column)
+    @moves[column].push(marker)
+    return @moves
+  end
+
+  def currentPlayerId
+    currentPlayer = currentPlayerId
+  end
+
+  def otherPlayerId
+    1 - @currentPlayerId
+  end
+
+  def switchPlayers
+    @currentPlayerId = otherPlayerId
+    puts "Player #{marker}\'s turn."
+    return @currentPlayerId
+  end
+
+end
+
+class Player
+  attr_accessor :id, :marker
+
+  def initialize
+    @id = id
+    @marker = marker
+  end
+end
+
+game = ConnectFour.new
---
+++ README.md
@@ -0,0 +1,12 @@
+The project is to create a command line version of the game Connect four. This project will be TDD-style so RSpec tests will be written before the code implementation. 
+
+The game has a six row, seven column board where the aim is to have form the first horizontal, vertical, or diagonal line of four. The pieces fall straight down, occupying the lowest available sapace within the column.
+
+
+| O | O | O | O | O | O | O |
+| O | O | O | O | O | O | O |
+| O | O | O | O | O | O | O |
+| O | O | O | O | O | O | O |
+| O | O | O | O | O | O | O |
+| O | O | O | O | O | O | O |
+
